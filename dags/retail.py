d367de5fa1428d10a83475b6d0e58818365eb028
Gabriel Fuentes <g_fuentes11@icloud.com>
2024-12-29 10:09:21 -0500

    Final Touches to Project

--- dags/retail.py
+++ dags/retail.py
@@ -1,6 +1,6 @@
 from airflow.decorators import dag, task
 from datetime import datetime
-
+from airflow.models.baseoperator import chain
 from airflow.providers.google.cloud.transfers.local_to_gcs import (
     LocalFilesystemToGCSOperator,
 )
@@ -11,6 +11,12 @@ from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+
+# needed for transform task
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 
 @dag(
@@ -46,6 +52,53 @@ def retail():
             name="raw_invoices", conn_id="gcp", metadata=Metadata(schema="retail")
         ),
         use_native_support=False,
+    )
+
+    @task.external_python(python="/usr/local/airflow/soda_venv/bin/python")
+    def check_load(scan_name="chuck_load", checks_subpath="sources"):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    transform = DbtTaskGroup(
+        group_id="transform",
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS, select=["path:models/transform"]
+        ),
+    )
+
+    @task.external_python(python="/usr/local/airflow/soda_venv/bin/python")
+    def check_transform(scan_name="check_transform", checks_subpath="transform"):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    report = DbtTaskGroup(
+        group_id="report",
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS, select=["path:models/report"]
+        ),
+    )
+
+    @task.external_python(python="/usr/local/airflow/soda_venv/bin/python")
+    def check_report(scan_name="check_report", checks_subpath="report"):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    chain(
+        upload_csv_to_gcs,
+        create_retail_dataset,
+        gcs_to_raw,
+        check_load(),
+        transform,
+        check_transform(),
+        report,
+        check_report(),
     )
 
 
